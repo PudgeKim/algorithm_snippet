4528e50d0e137dc55b93f078c5d09003fbb4fd68
JungMin <zxaq15@gmail.com>
2021-01-30 22:10:16 +0900

    clock modified

--- clock/clock_dist.py
+++ clock/clock_dist.py
@@ -7,3 +7,13 @@ def get_dist_from_clock(a, b):
     else:
         result = a - b
     return result
+
+# 시계가 아닌 원인 경우 n 인자를 추가
+
+def get_dist_from_clock(a, b, n):
+    a, b = max(a, b), min(a, b)
+    if a-b > n//2: # n이 자연수가 아닌 경우 n/2 고려
+        result = n - (a-b)
+    else:
+        result = a - b
+    return result
